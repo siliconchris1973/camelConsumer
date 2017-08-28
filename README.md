dc6cb499167471b97fd9f635f2e492ae498fbf93
Christian Günther <chris.guenther@mac.com>
2017-08-28 15:03:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CamelConsumer
+Basic test project for spring cloud stream sink
+
+https://dzone.com/articles/event-driven-microservices-using-spring-cloud-stre
+
+https://www.rabbitmq.com/reliability.html
+
+http://docs.spring.io/spring-integration/reference/html/amqp.html#amqp-inbound-ack
+
+http://www.rabbitmq.com/confirms.html
---
+++ src/main/java/siliconchris1973/Application.java
@@ -0,0 +1,17 @@
+// *********************************************************************************************************************
+// * (C) 2017 Immosolve GmbH, Tegelbarg 43, 24576 Bad Bramstedt
+// *********************************************************************************************************************
+
+package siliconchris1973;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
